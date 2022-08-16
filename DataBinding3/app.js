5eaef4e28116dbae5a6e6dc8f5cc97b52b3a52ec
shahriarshawon01 <shahriarshawon001@gmail.com>
2022-08-16 15:39:32 +0600

    Add data binding and attribute

--- DataBinding3/app.js
+++ DataBinding3/app.js
@@ -1,8 +1,14 @@
 var appSrs = Vue.createApp({
-    data(){
-        return {
-            msg: "Welcome to Vue Js 3"
-        };
-    }
+  data() {
+    return {
+      guideInfo: {
+        name: "Guide of Vue Js 3",
+        gitHubLink: "https://github.com/shahriarshawon01/VueJs",
+      },
+      vueGuide:
+      '<a href="https://vuejs.org/guide/quick-start.html#with-build-tools">VueGuide</a>',
+    };
+  },
 });
 appSrs.mount("#app");
+``
